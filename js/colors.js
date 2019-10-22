c87fd4f916d68ea7d72624549711161175c57028
HannekeHuls <hanneke.huls@gmail.com>
2019-10-22 16:51:23 +0200

    colors adapted and moved to new js file

---
+++ js/colors.js
@@ -0,0 +1,11 @@
+const ColorsLight = [
+    "#c4eded",//blauw
+    "#bce0bc",//groen
+    "#eef2bc",//geel
+];
+
+const ColorsDark = [
+    "#72b5b5",//blauw
+    "#6aa56a",//groen
+    "#ddea2a",//geel
+];
